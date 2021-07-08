0dde4af0560e2708180ac180a6305b6bcec4aa1d
Peter <peter@customdesign.co.za>
2021-07-08 14:00:54 +0200

    End of section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,7 +26,6 @@ namespace API.Controllers
             _userRepository = userRepository;
         }
 
-
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
